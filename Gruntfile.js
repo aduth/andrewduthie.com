4517b87345e859bc35a126778e5cf87bcc180337
Andrew Duthie <aduthie7@gmail.com>
2014-01-01 10:21:22 -0500

    Create "release" task for deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,12 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         siteConfig: grunt.file.readJSON('config.json'),
+        remote: {
+            host: 'andrewduthie.com',
+            username: 'deploy',
+            sitePath: '/home/deploy/andrewduthie.com',
+            privateKey: grunt.file.read(process.env.HOME + '/.ssh/id_rsa')
+        },
 
         sass: {
             dist: {
@@ -18,12 +24,70 @@ module.exports = function(grunt) {
                 files: ['public/css/sass/main.scss'],
                 tasks: ['sass']
             }
+        },
+
+        compress: {
+            release: {
+                options: {
+                    archive: 'output.tar.gz'
+                },
+                mode: 'tgz',
+                files: [{
+                    expand: true,
+                    cwd: '<%= siteConfig.output %>/',
+                    src: ['**'],
+                    dest: './'
+                }]
+            }
+        },
+
+        sftp: {
+            release: {
+                files: {
+                    './': 'output.tar.gz'
+                },
+                options: {
+                    path: '<%= remote.sitePath %>/',
+                    host: '<%= remote.host %>',
+                    username: '<%= remote.username %>',
+                    privateKey: '<%= remote.privateKey %>',
+                    srcBasePath: '<%= siteConfig.output %>/'
+                }
+            }
+        },
+
+        sshexec: {
+            decompress: {
+                command: 'tar -xzf <%= remote.sitePath %>/output.tar.gz -C <%= remote.sitePath %>/public_html/',
+                options: {
+                    host: '<%= remote.host %>',
+                    username: '<%= remote.username %>',
+                    privateKey: '<%= remote.privateKey %>'
+                }
+            },
+
+            removePackage: {
+                command: 'rm <%= remote.sitePath %>/output.tar.gz',
+                options: {
+                    host: '<%= remote.host %>',
+                    username: '<%= remote.username %>',
+                    privateKey: '<%= remote.privateKey %>'
+                }
+            }
+        },
+
+        clean: {
+            'post-release': ['output.tar.gz']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-ssh');
 
     grunt.registerTask('default', ['sass']);
     grunt.registerTask('start', ['default', 'watch']);
+    grunt.registerTask('release', ['compress', 'sftp', 'sshexec:decompress', 'sshexec:removePackage', 'clean:post-release']);
 };
