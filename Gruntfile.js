9cd6b5c73593d22297d9849cd750b05f636fcedb
Andrew Duthie <aduthie7@gmail.com>
2014-01-05 17:02:55 -0500

    Update to preferred 2 spaces (instead of 4)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,107 @@
 module.exports = function(grunt) {
-    var homePath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+  var homePath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 
-    grunt.initConfig({
-        siteConfig: grunt.file.readJSON('config.json'),
-        remote: {
-            host: 'andrewduthie.com',
-            username: 'deploy',
-            sitePath: '/home/deploy/andrewduthie.com',
-            privateKey: grunt.file.read(homePath + '/.ssh/id_rsa')
+  grunt.initConfig({
+    siteConfig: grunt.file.readJSON('config.json'),
+    remote: {
+      host: 'andrewduthie.com',
+      username: 'deploy',
+      sitePath: '/home/deploy/andrewduthie.com',
+      privateKey: grunt.file.read(homePath + '/.ssh/id_rsa')
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'public/css/main.css': ['public/css/sass/main.scss']
         },
-
-        sass: {
-            dist: {
-                files: {
-                    'public/css/main.css': ['public/css/sass/main.scss']
-                },
-                options: {
-                    style: 'compressed'
-                }
-            }
-        },
-
-        shell: {
-            generate: {
-                command: 'node build.js'
-            }
-        },
-
-        watch: {
-            sass: {
-                files: ['public/css/sass/*'],
-                tasks: ['sass', 'shell:generate']
-            },
-
-            blog: {
-                files: ['post/*'],
-                tasks: ['shell:generate']
-            }
-        },
-
-        compress: {
-            deploy: {
-                options: {
-                    archive: 'output.tar.gz'
-                },
-                mode: 'tgz',
-                files: [{
-                    expand: true,
-                    cwd: '<%= siteConfig.output %>/',
-                    src: ['**'],
-                    dest: './'
-                }]
-            }
-        },
-
-        sftp: {
-            deploy: {
-                files: {
-                    './': 'output.tar.gz'
-                },
-                options: {
-                    path: '<%= remote.sitePath %>/',
-                    host: '<%= remote.host %>',
-                    username: '<%= remote.username %>',
-                    privateKey: '<%= remote.privateKey %>',
-                    srcBasePath: '<%= siteConfig.output %>/'
-                }
-            }
-        },
-
-        sshexec: {
-            decompress: {
-                command: 'tar -xzf <%= remote.sitePath %>/output.tar.gz -C <%= remote.sitePath %>/public_html/',
-                options: {
-                    host: '<%= remote.host %>',
-                    username: '<%= remote.username %>',
-                    privateKey: '<%= remote.privateKey %>'
-                }
-            },
-
-            removePackage: {
-                command: 'rm <%= remote.sitePath %>/output.tar.gz',
-                options: {
-                    host: '<%= remote.host %>',
-                    username: '<%= remote.username %>',
-                    privateKey: '<%= remote.privateKey %>'
-                }
-            }
-        },
-
-        clean: {
-            'post-deploy': ['output.tar.gz']
+        options: {
+          style: 'compressed'
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-ssh');
+    shell: {
+      generate: {
+        command: 'node build.js'
+      }
+    },
 
-    grunt.registerTask('default', ['sass']);
-    grunt.registerTask('dev', ['default', 'watch']);
-    grunt.registerTask('deploy', ['shell:generate', 'compress', 'sftp', 'sshexec:decompress', 'sshexec:removePackage', 'clean:post-deploy']);
+    watch: {
+      sass: {
+        files: ['public/css/sass/*'],
+        tasks: ['sass', 'shell:generate']
+      },
+
+      blog: {
+        files: ['post/*'],
+        tasks: ['shell:generate']
+      }
+    },
+
+    compress: {
+      deploy: {
+        options: {
+          archive: 'output.tar.gz'
+        },
+        mode: 'tgz',
+        files: [{
+          expand: true,
+          cwd: '<%= siteConfig.output %>/',
+          src: ['**'],
+          dest: './'
+        }]
+      }
+    },
+
+    sftp: {
+      deploy: {
+        files: {
+          './': 'output.tar.gz'
+        },
+        options: {
+          path: '<%= remote.sitePath %>/',
+          host: '<%= remote.host %>',
+          username: '<%= remote.username %>',
+          privateKey: '<%= remote.privateKey %>',
+          srcBasePath: '<%= siteConfig.output %>/'
+        }
+      }
+    },
+
+    sshexec: {
+      decompress: {
+        command: 'tar -xzf <%= remote.sitePath %>/output.tar.gz -C <%= remote.sitePath %>/public_html/',
+        options: {
+          host: '<%= remote.host %>',
+          username: '<%= remote.username %>',
+          privateKey: '<%= remote.privateKey %>'
+        }
+      },
+
+      removePackage: {
+        command: 'rm <%= remote.sitePath %>/output.tar.gz',
+        options: {
+          host: '<%= remote.host %>',
+          username: '<%= remote.username %>',
+          privateKey: '<%= remote.privateKey %>'
+        }
+      }
+    },
+
+    clean: {
+      'post-deploy': ['output.tar.gz']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-ssh');
+
+  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('dev', ['default', 'watch']);
+  grunt.registerTask('deploy', ['shell:generate', 'compress', 'sftp', 'sshexec:decompress', 'sshexec:removePackage', 'clean:post-deploy']);
 };
