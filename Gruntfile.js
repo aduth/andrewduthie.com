033706db7eed316e17585f4e739e7144acf670b4
Andrew Duthie <aduthie7@gmail.com>
2014-02-06 20:19:17 -0500

    Watch correct LESS styles path

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
 
     watch: {
       less: {
-        files: ['public/css/less/**/*.less'],
+        files: ['public/assets/css/less/**/*.less'],
         tasks: ['less', 'copy:assets']
       },
 
