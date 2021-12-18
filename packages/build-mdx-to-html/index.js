c6521bb70a7a36a8e0443a0a3843e09958126c5a
Andrew Duthie <andrew@andrewduthie.com>
2021-12-18 13:16:44 -0500

    Convert from worker eval to temp file import
    Avoid dealing with all the nonsense of making the worker work.
    
    Specifically: The worker evaluates in the context of the current working directory, not the package, so it may fail to resolve dependencies of build-mdx-to-html (e.g. React), depending how NPM chooses to dedupe the package tree. I could not sort out how to force it to resolve as though it were in the package.

--- packages/build-mdx-to-html/index.js
+++ packages/build-mdx-to-html/index.js
@@ -1,7 +1,19 @@
 import { dirname } from 'path';
-import { Worker } from 'worker_threads';
-import mdx from '@mdx-js/mdx';
+import { createHash } from 'crypto';
+import { writeFile, unlink } from 'fs/promises';
+import { compile } from '@mdx-js/mdx';
 import esbuild from 'esbuild';
+
+/** @typedef {esbuild.Metafile} Metafile */
+
+/**
+ * Returns an MD5 hash of the given string.
+ *
+ * @param {string} string String to hash.
+ *
+ * @return {string} Hashed string.
+ */
+const md5 = (string) => createHash('md5').update(string).digest('hex');
 
 /**
  * @type {import('@aduth/build-files').Builder}
@@ -9,16 +21,17 @@ import esbuild from 'esbuild';
 async function build(source, options) {
 	const { file } = options;
 
-	const jsx = await mdx(source);
-	const { outputFiles } = await esbuild.build({
+	const jsx = String(await compile(source, { jsxRuntime: 'classic' }));
+	const { outputFiles, metafile } = await esbuild.build({
 		stdin: {
 			contents: jsx,
 			loader: 'jsx',
 			resolveDir: dirname(file),
 		},
+		metafile: true,
 		bundle: true,
 		write: false,
-		format: 'cjs',
+		format: 'esm',
 		platform: 'node',
 		plugins: [
 			{
@@ -33,22 +46,23 @@ async function build(source, options) {
 		],
 	});
 	const code = outputFiles.map(({ text }) => text).join('');
-	const worker = new Worker(
-		`const { mdx } = require('@mdx-js/react');
-		const React = require('react');
-		const { renderToStaticMarkup } = require('react-dom/server');
-		const { MDXProvider } = require('@mdx-js/react');
-		const { parentPort, workerData } = require('worker_threads');
-		${code}
-		const { default: CompiledComponent, ...props } = exports;
-		const element = React.createElement(CompiledComponent, props);
-		const elementWithProvider = React.createElement(MDXProvider, null, element);
-		const rendered = '<!doctype html>' + renderToStaticMarkup(elementWithProvider);
-		parentPort.postMessage(rendered);`,
-		{ eval: true }
-	);
+	const namedExports = Object.values(/** @type {Metafile} */ (metafile).outputs)
+		.flatMap((output) => output.exports)
+		.filter((name) => name !== 'default');
 
-	return new Promise((resolve) => worker.on('message', resolve));
+	const tempFile = new URL(`./tmp/${md5(code)}.js`, import.meta.url);
+	await writeFile(
+		tempFile,
+		`${code}
+		import React from 'react';
+		import { renderToStaticMarkup } from 'react-dom/server.js';
+		const element = React.createElement(MDXContent, {${namedExports.join(',')}});
+		export const _builtMDXContent = '<!doctype html>' + renderToStaticMarkup(element);`
+	);
+	const { _builtMDXContent: content } = await import(tempFile.toString());
+	await unlink(tempFile);
+
+	return content;
 }
 
 export default build;
